626a2ccafe3d2b17837a5a5acac4e7692b275031
admin <1143555752@qq.com>
2020-12-27 19:02:30 +0800

    把idea工程添加到本地仓库

---
+++ git_project/src/cn/itcast/git/Firstgittest.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+public class Firstgittest {
+    public static void main(String[] args) {
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+    }
+}
